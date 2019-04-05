c447c887c0da711d4499e0bf461917292425e936
DenisPavlov <denispavlov1989@gmail.com>
2019-04-05 13:56:41 +1000

    first commit

---
+++ src/main/java/classloaders/Main.java
@@ -0,0 +1,45 @@
+package classloaders;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) throws MalformedURLException, ClassNotFoundException {
+        Person person = new Person();
+        ClassLoader classLoader = person.getClassLoader();
+        System.out.println(classLoader);
+
+        //у classLoader еще нет класса Car
+
+        Car car = new Car();
+
+        // а тут уже есть
+
+        ClassLoader parentClassLoader = classLoader.getParent();
+        System.out.println(parentClassLoader);
+
+
+//        URLClassLoader pluginClassLoader = new URLClassLoader(new URL[]{new URL("file:dsadsa")});
+//        Class cl = pluginClassLoader.loadClass("myClass");
+    }
+}
+
+class MyLoader extends ClassLoader {
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] bytes = new byte[0];
+        try {
+            bytes = Files.readAllBytes(Paths.get("fileName"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Class<?> cl = defineClass(name, bytes, 0, bytes.length);
+        return cl;
+    }
+}
---
+++ src/main/java/classloaders/Person.java
@@ -0,0 +1,17 @@
+package classloaders;
+
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ClassLoader getClassLoader(){
+        return getClass().getClassLoader();
+    }
+}
